23e4ca7db4898be1738f80e8f2a990d5a6cc2da4
KJackson50 <KJackson5053@gmail.com>
2018-01-19 21:43:23 -0500

    Update2
    tested the int-string-back to int

--- FindTheCool.java
+++ FindTheCool.java
@@ -15,12 +15,22 @@ public class FindTheCool {
 		}
 		
 		//example of s.charAt() in action
-		System.out.println(s.charAt(0));
+		//System.out.println(s.charAt(0));
 		//example of Character.getNum in action
-		System.out.println(Character.getNumericValue(s.charAt(0)));
+		//System.out.println(Character.getNumericValue(s.charAt(0)));
 		
+		int sum;
 		int digit1 = Character.getNumericValue(s.charAt(0));
+		int digit2 = Character.getNumericValue(s.charAt(1));
 		System.out.println(digit1);
+		System.out.println(digit2);
+		
+		sum = digit1 + digit2;
+		
+		System.out.println(sum);
+		System.out.println(s.length());
+		
+		
 
 	}
 
