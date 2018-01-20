4d7ff2213535b888e73e280d6eecceb25d22acfa
KJackson50 <KJackson5053@gmail.com>
2018-01-20 01:05:39 -0500

    Update3 - Core structure
    Here we have the core structure of the program.  This will be merged into the master.

--- FindTheCool.java
+++ FindTheCool.java
@@ -3,18 +3,54 @@ public class FindTheCool {
 
 	public static void main(String[] args) {
 
-
-		long num = 123456789;
-		int count = 0;
-		String s = String.valueOf(num);
-		for(int i = 0; i < s.length(); i++)
-		{
-		 char ch = s.charAt(i);
-		 if(ch == '1')
-		    count ++;
-		}
-		System.out.println(s.charAt(0));
-
+		int coolSum;
+		
+		coolSum = getCoolNum();
+		
+		System.out.println("The sum equals: " + coolSum);
+		
+		
+	
 	}
+
+	public static int getCoolNum() {
+		int sum = 0;
+		int total = 0;
+		int uncoolCount = 0;
+		boolean exit = false;
+		
+do {
+		for(int i=1; i<=23; i++) {
+			
+			String s = String.valueOf(i);
+			
+			for(int x=0; x<s.length(); x++) {
+				//char ch = s.charAt(x);
+				int digit = Character.getNumericValue(s.charAt(x));
+				
+				sum += digit * digit;
+				
+				System.out.println(sum);
+			}
+		if(sum == 1) {
+				total += i;
+				exit = false;
+			}
+			else if(sum == 4) {
+				uncoolCount++;	
+				exit = false;
+			}
+			else if (i == 23) {
+				exit= true;
+			}
+		}
+}while(exit = false);
+
+		return total;		
+		
+		
+	}
+	
+	
 
 }
